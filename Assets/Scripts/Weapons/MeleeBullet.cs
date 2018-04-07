3b4f9e767e61098da78a59f625798a0dbbfb4149
Stanislav Ivanaev <ivanaev@skbkontur.ru>
2018-04-08 00:55:05 +0500

    start to implement weapon system

---
+++ Assets/Scripts/Weapons/MeleeBullet.cs
@@ -0,0 +1,26 @@
+﻿public abstract class MeleeBullet : MovingObject
+{
+	private const int TtlLimit = 2;
+	private int ttl;
+
+	private float _objectSpeed = 0.1f;
+
+	public abstract int Damage { get; }
+	
+	// Use this for initialization
+
+	// Update is called once per frame
+	void Update ()
+	{
+		ttl++;
+		if (ttl >= TtlLimit)
+		{
+			Destroy(this);
+		}
+	}
+
+	protected override float ObjectSpeed
+	{
+		get { return _objectSpeed; }
+	}
+}
--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -1,9 +1,17 @@
-﻿namespace Weapons
+﻿using UnityEngine;
+
+namespace Weapons
 {
-    public abstract class Weapon
+    public abstract class Weapon: MonoBehaviour
     {
+        protected abstract MeleeBullet Bullet { get; }
         public string Name { get; set; }
-        public abstract float Attack { get; set; }
+        public abstract float AttackValue { get; set; }
         public bool IsCurrent { get; set; }
+
+        public void Attack()
+        {
+            Instantiate(Bullet);
+        }
     }
 }
