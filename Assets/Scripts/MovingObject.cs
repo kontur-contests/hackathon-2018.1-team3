1acf2f02bc5b2a06501e41277a875c260bad18ec
Stanislav Ivanaev <ivanaev@skbkontur.ru>
2018-04-07 18:10:50 +0500

    rewrite some methods

--- Assets/Scripts/MovingObject.cs
+++ Assets/Scripts/MovingObject.cs
@@ -6,12 +6,18 @@ public abstract class MovingObject: MonoBehaviour
 
     protected void MoveObject(Rigidbody2D rb, Vector2 movement)
     {
-        //transform.Translate(movement);
         rb.MovePosition(rb.position + movement);
     }
 
     protected float GetMovementMultiplier()
     {
         return ObjectSpeed * Time.deltaTime;
+    }
+    
+    protected Vector3 CalculateNecessaryMovementValues(Vector3 otherMovement)
+    {
+        var currentPosition = transform.position;
+        var multiplier = GetMovementMultiplier();
+        return multiplier * (otherMovement - currentPosition);
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,22 +4,41 @@ using UnityEngine;
 public class Player : MovingObject
 {
     private Rigidbody2D rb2d;
+    private Collider2D c2d;
     
     protected override float ObjectSpeed
     {
-        get { return 10.0f; }
+        get { return 5.0f; }
     }
 
     // Use this for initialization
 	void Start ()
 	{
 	    rb2d = GetComponent<Rigidbody2D>();
+	    c2d = GetComponent<Collider2D>();
 	}
 	
 	// Update is called once per frame
     void Update()
     {
         AttemptToMove();
+    }
+    
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            var center = transform.position;
+            var otherCenter = other.transform.position;
+            if ((center - otherCenter).Length() <= c2d.bounds.extents.Length() + 0.2)
+                rb2d.velocity = new Vector2(0, 0);
+
+            else
+            {
+                var necessaryMovement = CalculateNecessaryMovementValues(otherCenter);
+                MoveObject(rb2d, necessaryMovement);
+            }
+        }
     }
 
     private void AttemptToMove()
