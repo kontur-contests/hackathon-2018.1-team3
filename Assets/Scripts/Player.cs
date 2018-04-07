d2c3f2423c796ceae4f2ab80a954bc6d4a8b0f9f
Stanislav Ivanaev <ivanaev@skbkontur.ru>
2018-04-07 14:55:58 +0500

    add player movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,41 @@
+﻿using Assets.Scripts.Extensions;
+using Assets.Scripts.Models;
+using UnityEngine;
+
+public class Player : MovingObject
+{
+    protected override float ObjectSpeed
+    {
+        get { return 10.0f; }
+    }
+
+    // Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        AttemptToMove();
+    }
+
+    private void AttemptToMove()
+    {
+        var currentMovement = GetCurrentMovement();
+        if (currentMovement.HasMovementAtAnyAxis())
+            MoveObject(currentMovement);
+    }
+
+    private CurrentMovement GetCurrentMovement()
+    {
+        var xMovement = ObjectSpeed * Time.deltaTime * Input.GetAxisRaw("Horizontal");
+        var yMovement = ObjectSpeed * Time.deltaTime * Input.GetAxisRaw("Vertical");
+
+        return new CurrentMovement
+        {
+            XAxisMovement = xMovement,
+            YAxisMovement = yMovement
+        };
+    }
+}
