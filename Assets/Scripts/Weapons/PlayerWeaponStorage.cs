ea1625f534dc22a9d74658b743b7d840be23749f
Stanislav Ivanaev <ivanaev@skbkontur.ru>
2018-04-07 19:57:59 +0500

    add weapons storage

---
+++ Assets/Scripts/Weapons/PlayerWeaponStorage.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+
+namespace Weapons
+{
+    public class PlayerWeaponStorage
+    {
+        private HashSet<Weapon> playerWeapons;
+
+        public PlayerWeaponStorage()
+        {
+            playerWeapons = new HashSet<Weapon>();
+        }
+
+        public PlayerWeaponStorage(HashSet<Weapon> playerWeapons)
+        {
+            this.playerWeapons = playerWeapons;
+        }
+
+        public HashSet<Weapon> GetAllPlayerWeapons()
+        {
+            return playerWeapons;
+        }
+
+        public Weapon GetPlayerWeapon(int id)
+        {
+            return playerWeapons.FirstOrDefault(w => w.Id == id);
+        }
+
+        public Weapon GetCurrentPlayerWeapon()
+        {
+            return playerWeapons.FirstOrDefault(w => w.IsCurrent);
+        }
+
+        public void UpdateWeapon(Weapon weapon)
+        {
+            var weaponInStorage = GetPlayerWeapon(weapon.Id);
+            if (weaponInStorage != null)
+                playerWeapons.Remove(weaponInStorage);
+            playerWeapons.Add(weapon);
+        }
+
+        public void SetCurrentPlayerWeapon(Weapon weapon)
+        {
+            var currentPlayerWeapon = GetCurrentPlayerWeapon();
+            currentPlayerWeapon.IsCurrent = false;
+            var newCurrentPlayerWeapon = GetPlayerWeapon(weapon.Id);
+            if (newCurrentPlayerWeapon == null)
+            {
+                playerWeapons.Add(weapon);
+            }
+            else
+            {
+                newCurrentPlayerWeapon.IsCurrent = true;
+            }
+        }
+    }
+}
