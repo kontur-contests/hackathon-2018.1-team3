169d23ce9d2cbbdcbfebc44daeb09e5e1c953ead
Островский Сергей <black.phoenix4466@gmail.com>
2018-04-07 21:51:10 +0500

    added map start

---
+++ Assets/Scripts/LevelMap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class LevelMap : MonoBehaviour {
+
+    Location[,] locations;
+    
+    LevelMap (string composition)
+    {
+        if (composition == "comp1")
+            locations = new Location[5,5];
+        locations[0, 0] = "scene1";
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
