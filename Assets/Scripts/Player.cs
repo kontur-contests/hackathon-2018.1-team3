6692be30dfb79627e984385d10acc457b9b5ade6
Островский Сергей <black.phoenix4466@gmail.com>
2018-04-07 18:25:14 +0500

    added GUI prototype

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Player : MovingObject
 {
+
+    public int health=100;
+    public int maxHealth;
+    //public string weapon1;
+    //public string weapon2;
+    public int strength=1;
+    public int agility=1;
+    public int endurance=1;
+
     protected override float ObjectSpeed
     {
         get { return 10.0f; }
@@ -12,6 +21,7 @@ public class Player : MovingObject
     // Use this for initialization
 	void Start ()
 	{
+        maxHealth = 50 + 50 * endurance;
 	}
 	
 	// Update is called once per frame
