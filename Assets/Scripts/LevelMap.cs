2f8a5a868f000bbe5a81de7897c1c83db2d8f68c
Arsel_Muginov <arsel.muginov@gmail.com>
2018-04-08 06:24:04 +0500

    Fixed room change

--- Assets/Scripts/LevelMap.cs
+++ Assets/Scripts/LevelMap.cs
@@ -1,29 +1,25 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using System;
 
 public class LevelMap 
 {
     MyLocation[,] locations;
     private int gridSize = 25;
+
     public LevelMap(string composition)
     {
-
         if (composition == "comp1")
         {
-
             locations = new MyLocation[gridSize, gridSize];
             //pretty random level 1;
-            locations[0, 0] = new MyLocation("scene0", up: false, down: false, left: false, right: true);
-            locations[1, 0] = new MyLocation("scene1", up: false, down: false, left: false, right: true);
-            locations[2, 0] = new MyLocation("scene2", up: false, down: false, left: false, right: true);
-            locations[2, 1] = new MyLocation("scene3", up: false, down: false, left: false, right: true);
-            locations[3, 1] = new MyLocation("scene4", up: false, down: false, left: false, right: true);
-            locations[4, 1] = new MyLocation("scene5", up: false, down: false, left: false, right: true);
-            locations[3, 0] = new MyLocation("scene6", up: false, down: false, left: false, right: true);
-            locations[3, 2] = new MyLocation("scene7", up: false, down: false, left: false, right: true);//reward room?
-            locations[3, 3] = new MyLocation("scene8", up: false, down: false, left: false, right: true);
+            locations[0, 0] = new MyLocation("Pool1-Monster-4way", up: false, down: false, left: false, right: true);
+            locations[1, 0] = new MyLocation("Pool1-Monster-4way", up: false, down: false, left: false, right: true);
+            locations[2, 0] = new MyLocation("Pool1-Monster-4way", up: false, down: false, left: false, right: true);
+            locations[2, 1] = new MyLocation("Pool1-Monster-4way", up: false, down: false, left: false, right: true);
+            locations[3, 1] = new MyLocation("Pool1-Monster-4way", up: false, down: false, left: false, right: true);
+            locations[4, 1] = new MyLocation("Pool1-Monster-4way", up: false, down: false, left: false, right: true);
+            locations[3, 0] = new MyLocation("Pool1-Monster-4way", up: false, down: false, left: false, right: true);
+            locations[3, 2] = new MyLocation("Pool1-Monster-4way", up: false, down: false, left: false, right: true);//reward room?
+            locations[3, 3] = new MyLocation("Pool1-Monster-4way", up: false, down: false, left: false, right: true);
             locations[5, 1] = new MyLocation("sceneBoss1", up: false, down: false, left: false, right: true);
             locations[6, 1] = new MyLocation("sceneShop1", up: false, down: false, left: false, right: true);
         }
@@ -31,23 +27,20 @@ public class LevelMap
 
     public string GetRoomNameByCoords(int i, int j)
     {
-        if (i >= 0 && i < gridSize)
-            return locations[i, j].sceneName;
-        else
+        try
         {
-            Debug.Log("Tried to visit incorrect room");
+            if (locations[i, j] != null)
+            {
+                return locations[i, j].sceneName;
+            }
+            else
+            {
+                return "";
+            }
+        }
+        catch (IndexOutOfRangeException)
+        {
             return "";
         }
-    }
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
     }
 }
