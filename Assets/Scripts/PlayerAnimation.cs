d9954128284073019b452a8ab7a45c25636469cc
Penguiny <maria.aschihmina@yandex.ru>
2018-04-08 05:33:53 +0500

    items

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+
+        anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKey(KeyCode.W))
+        {
+            anim.Play("SimpleUp");
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            anim.Play("SimpleRight");
+        }
+        else if(Input.GetKey(KeyCode.S))
+        {
+            anim.Play("SimpleDown");
+        }
+        else if(Input.GetKey(KeyCode.A))
+        {
+            anim.Play("SimpleLeft");
+        }
+
+	}
+}
